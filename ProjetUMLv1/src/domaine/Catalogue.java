45485592606a0aaa592b8b2339ed19bbd2e43a78
AxelClm <46006583+AxelClm@users.noreply.github.com>
2019-12-13 15:21:51 -0500

    Interface prend en compte la BDD

--- ProjetUMLv1/src/domaine/Catalogue.java
+++ ProjetUMLv1/src/domaine/Catalogue.java
@@ -3,14 +3,14 @@ import java.util.*;
 public class Catalogue {
 	private LinkedList<Produit> ListeProd;
 	private LinkedList<Categorie> ListeCategorie;
-	Catalogue(){
+	public Catalogue(){
 		update();
 	}
-	synchronized void update() {
+	public synchronized void update() {
 		this.ListeCategorie = bdd.BddCatalogue.getCategories();
 		this.ListeProd=bdd.BddCatalogue.getProduits();
 	}
-	LinkedList<Produit> getProduitByCategorie(int idCategorie) {
+	public LinkedList<Produit> getProduitByCategorie(int idCategorie) {
 		LinkedList<Produit> ListePbC = new LinkedList<Produit>();
 		ListeProd.forEach(p -> {
 			if(p.getCategorie() == idCategorie) {
